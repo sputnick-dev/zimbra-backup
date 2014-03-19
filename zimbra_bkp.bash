23f1aa2c710abedb56a4e0cf9668ddb89d59e450
sputnick <gilles.quenot@gmail.com>
2014-03-19 22:36:37 +0100

    now using config variables

--- zimbra_bkp.bash
+++ zimbra_bkp.bash
@@ -4,21 +4,23 @@
 
 # In // of this script, a FS backup is recommended with rsync by example.
 
+zimbra_backup_path=/home/backup/zimbra
+mysql_backup_path=/home/backup/bdd/mysql
 mysql_root_pw=$(/opt/zimbra/bin/zmlocalconfig -s | awk '$1 == "mysql_root_password"{print $3}')
 
-mkdir -p /home/backup/zimbra
+mkdir -p $zimbra_backup_path
 
 # mails+metadatas+calendars+todos
 for account in $(su - zimbra -c 'zmprov -l getAllAccounts' | egrep -v '^spam|^ham|^virus-quarantine'); do
-    su - zimbra -c "zmmailbox -z -m $account getRestURL '//?fmt=tgz'" > /home/backup/zimbra/mails+cal+todos_${account}_$(date +%Y%m%d-%H%M).tgz
+    su - zimbra -c "zmmailbox -z -m $account getRestURL '//?fmt=tgz'" > $zimbra_backup_path/mails+cal+todos_${account}_$(date +%Y%m%d-%H%M).tgz
 done
 
 # ldap
 su - zimbra -c "/opt/zimbra/libexec/zmslapcat /tmp/"
-\mv /tmp/ldap.bak.* /home/backup/zimbra
+\mv /tmp/ldap.bak.* $zimbra_backup_path
 
 # mysql -> http://wiki.zimbra.com/wiki/MySQL_Backup_and_Restore
-/opt/zimbra/mysql/bin/mysqldump --user=root --password=$mysql_root_pw --socket=/opt/zimbra/db/mysql.sock --all-databases --single-transaction --flush-logs > /home/backup/bdd/mysql/zimbra_dump-$(date +%Y%m%d%H%M).sql.gz
+/opt/zimbra/mysql/bin/mysqldump --user=root --password="$mysql_root_pw" --socket=/opt/zimbra/db/mysql.sock --all-databases --single-transaction --flush-logs > $mysql_backup_path/zimbra_dump-$(date +%Y%m%d%H%M).sql.gz
 
 # clean
-find /home/backup/zimbra -type f -mtime +15 -delete 2>/dev/null
+find $zimbra_backup_path -type f -mtime +15 -delete 2>/dev/null
